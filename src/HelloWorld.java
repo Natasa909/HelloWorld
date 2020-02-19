8f142b1169e9f55214f7779aa66bcdc6859c9bd4
Natasa909 <natasakoncarevic@outlook.com>
2020-02-19 21:17:55 +0100

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.print("Testing");
+		System.out.print(" app");
+		System.out.println(" with Selenium.");
+		System.out.println("Testing app with Selenium.");
+		System.out.println("Save with CTRL+S.");
+		System.out.println(123);
+		
+
+	}
+
+}
